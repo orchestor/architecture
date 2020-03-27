b04811f4fd7950f516f488961f0dc0516e08e059
Lin Wang <orchestor@gmail.com>
2020-03-27 08:28:03 -0700

    add oop as a new folder since more concepts need to demo

---
+++ paradigm/object_oriented_programming/class.py
@@ -0,0 +1,12 @@
+class Character:
+    def __init__(self, name, initial_health):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(self.name + " is sitting!")
+
+
+
+# Object oriented programming is a programming paradigm that uses objects to solve the real world problems. We use class to define new types of objects. A class has three important concepts: encapsulation, inheritance and polimorphism. 
+# 
